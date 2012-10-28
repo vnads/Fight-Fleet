aba7cc0f792318625b36f973dbde75fc692bef21
shamidi <shamidi@luc.edu>
2012-10-28 16:58:29 -0500

    Added Post Logic in the form of a PostTask to the Concept Activity.

--- FightFleetClient/src/com/fightfleet/fightfleetclient/ConceptActivity.java
+++ FightFleetClient/src/com/fightfleet/fightfleetclient/ConceptActivity.java
@@ -2,8 +2,11 @@ package com.fightfleet.fightfleetclient;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
@@ -27,20 +30,26 @@ public class ConceptActivity extends Activity {
     }
     
     public void testGet(View view) {
-    	try
-    	{
+    	try {
 	    	GetTask getTask = new GetTask();
 	    	URL createUser = new URL("http://fightfleetapi.apphb.com/user/createuser");
 	    	getTask.execute(createUser);
     	}
-    	catch (Exception ex)
-    	{
-    		
+    	catch (Exception ex){
+    	    		
     	}
     }
     
     public void testPost(View view){
-    	
+    	try{
+    		PostTask postTask = new PostTask();
+    		//URL url = new URL("http://fightfleetapi.apphb.com/user/testpost");
+    		URL url = new URL("http://posttestserver.com/post.php");
+    		postTask.execute(url);
+    	}
+    	catch (Exception ex){
+    		
+    	}
     }
     
     private class GetTask extends AsyncTask<URL, Void, String> {
@@ -74,6 +83,47 @@ public class ConceptActivity extends Activity {
           View v= findViewById(R.id.textViewResult);
           TextView txtVw = (TextView)v;
           txtVw.setText(result);
+    	}
+    }
+    private class PostTask extends AsyncTask<URL, Void, String> {
+    	
+    	@Override
+    	protected String doInBackground(URL... params) {
+			try {
+				StringBuilder sb = new  StringBuilder();
+			    // Construct data
+			    String data = URLEncoder.encode("test", "UTF-8") + "=" + URLEncoder.encode("value1", "UTF-8");
+			    //data += "&" + URLEncoder.encode("key2", "UTF-8") + "=" + URLEncoder.encode("value2", "UTF-8");
+
+			    // Send data
+			    for (int i = 0; i < params.length;i++){
+			    	URL url = params[i];
+				    URLConnection conn = url.openConnection();
+				    conn.setDoOutput(true);
+				    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+				    wr.write(data);
+				    wr.flush();
+	
+				    // Get the response
+				    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				    String line;
+				    while ((line = rd.readLine()) != null) {
+				        sb.append(line);
+				    }
+				    wr.close();
+				    rd.close();
+			    }
+			    return sb.toString();
+			} catch (Exception e) {
+				return "There was an error.\n"+ e.getMessage() + "\n" + e.getStackTrace();
+			}
+    	}
+    	
+    	@Override
+    	protected void onPostExecute(String result){
+    	    View v= findViewById(R.id.textViewResult);
+            TextView txtVw = (TextView)v;
+            txtVw.setText(result);
     	}
     }
 }
