febe4f3855543128301b37a9f4efc546abd4ec39
shamidi <shamidi@luc.edu>
2012-10-29 21:14:50 -0500

    Added a license and a MainActivity which has nothing included in it yet.

--- FightFleetClient/gen/com/fightfleet/fightfleetclient/R.java
+++ FightFleetClient/gen/com/fightfleet/fightfleetclient/R.java
@@ -23,17 +23,21 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_concept=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
         public static final int activity_concept=0x7f060000;
+        public static final int activity_main=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int get_button_text=0x7f040005;
+        public static final int hello_world=0x7f040007;
         public static final int menu_settings=0x7f040001;
         public static final int post_button_text=0x7f040004;
         public static final int result_string=0x7f040006;
         public static final int title_activity_concept=0x7f040002;
+        public static final int title_activity_main=0x7f040008;
         public static final int welcome=0x7f040003;
     }
     public static final class style {
