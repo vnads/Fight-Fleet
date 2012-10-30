44d5f5bbb5958e5391c807b5b30b3e9c7f45c7d1
Vamsi Nadimpalli <vamsi.nadimpalli@gmail.com>
2012-10-30 17:46:29 -0500

    adding manager classes, beginning of game business logic

---
+++ FightFleetApi/FightFleet/Managers/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FightFleet.Models;
+
+namespace FightFleet.Managers
+{
+    public class GameManager
+    {
+        //public IEnumerable<UserGameModel> GetGamesForUser(int userId)
+        //{
+        //    using(var 
+        //}
+    }
+}
---
+++ FightFleetApi/FightFleet/Models/AuthenticationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FightFleet.Models
+{
+    public class AuthenticationManager
+    {
+        public bool IsValid(int userId, Guid accessToken)
+        {
+            using (var ctx = new FightFleetDataContext())
+            {
+                var token = ctx.Authentications.FirstOrDefault(c => c.UserId == userId && c.AccessToken == accessToken);
+                if (token == null || token.ExpiresOn <= DateTime.Now)
+                    return false;
+
+                return true;
+            }
+        }   
+    }
+}
