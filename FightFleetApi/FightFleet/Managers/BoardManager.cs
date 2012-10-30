bd451884dbe5179ce7856fff7b85b3265f3a6bdb
Vamsi Nadimpalli <vamsi.nadimpalli@gmail.com>
2012-10-30 18:40:42 -0500

    adding skeleton for board generation

---
+++ FightFleetApi/FightFleet/Managers/BoardManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FightFleet.Models;
+
+namespace FightFleet.Managers
+{
+    public class BoardManager
+    {
+        public BoardModel CreateRandomBoard()
+        {
+            //generate randomized xml
+
+            //save to database
+
+
+            return new BoardModel();
+        }
+    }
+}
