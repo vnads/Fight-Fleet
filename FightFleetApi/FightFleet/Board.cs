f7660589ff5c55f836119b07200d94528a996fb8
Akrem Osman <akrem1st@hotmail.com>
2012-11-01 20:31:08 -0500

    Construct the Board class and Write a new class that randomly generates a board. Placing 5 ships randomly into the board

--- FightFleetApi/FightFleet/Board.cs
+++ FightFleetApi/FightFleet/Board.cs
@@ -5,7 +5,37 @@ using System.Text;
 
 namespace FightFleet
 {
-    partial class Board
+    public class GameBoard
     {
+        public const int XSIZE = 10;
+        public const int YSIZE = 10;
+
+        public CellStatus[,] BoardCells { get; set; }
+
+        public GameBoard()
+        {
+            BoardCells = new CellStatus[XSIZE, XSIZE]; 
+
+            for (int i = 0; i < XSIZE; i++) {
+                for (int j = 0; j < XSIZE; j++) {
+                    BoardCells[i, j] = CellStatus.Blank;
+                }
+            }
+        }
+    }
+    
+    public enum CellStatus
+    {
+        Blank,
+        Ship,
+        Hit
+    }
+
+    public enum ShipDirections
+    {
+        Up = 1,
+        Right = 2,
+        Down = 3,
+        Left = 4
     }
 }
